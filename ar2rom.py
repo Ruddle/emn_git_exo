ddfd8b2ac1b3296eb9826abdb92776a9d7710f98
Thomas SIMON <thomas.simon.work@gmail.com>
2016-10-12 13:41:14 +0200

    Capitalized some letters and solve 2nd conflict

--- ar2rom.py
+++ ar2rom.py
@@ -1,11 +1,11 @@
 import unittest
 
 conversion_map = (
-    ('x', 10),
-    ('ix', 9),
-    ('v', 5),
-    ('iv', 4),
-    ('i', 1))
+    ('X', 10),
+    ('IX', 9),
+    ('V', 5),
+    ('IV', 4),
+    ('I', 1))
 
 
 def ar2rom(val):
@@ -34,16 +34,11 @@ class Tests(unittest.TestCase):
     def test_5_to_V(self):
         self.assertEqual(ar2rom(5), 'V')
 
-<<<<<<< 8135948a07fe760921732f0364eb0b99d7cf7a8c
-    def test_9_to_ix(self):
-        self.assertEqual(ar2rom(9), 'ix')
+    def test_9_to_IX(self):
+        self.assertEqual(ar2rom(9), 'IX')
 
-    def test_10_to_x(self):
-        self.assertEqual(ar2rom(10), 'x')
-=======
     def test_10_to_X(self):
         self.assertEqual(ar2rom(10), 'X')
->>>>>>> Capitalizing roman letter
 
     def test_20_to_XX(self):
         self.assertEqual(ar2rom(20), 'XX')
